e0fd804aae4a1d0bea6c18d6481c1adea5529c79
amirnsamimi <amirnsamimi@gmail.com>
2024-09-16 19:27:58 +0330

    project done

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -4,7 +4,7 @@ import Tags from "@/components/Tags.vue";
 import { useFetch } from "@/composables/useFetch";
 import { API_CONFIG, MOVIE_APIS } from "@/config/api";
 import type { ISINGLEMOVIE } from "@/lib/definitions";
-import { computed, onMounted, ref, watch } from "vue";
+import { onMounted, ref, watch } from "vue";
 import { useRoute } from "vue-router";
 import RatingCircle from "@/components/RatingCircle.vue";
 
@@ -62,7 +62,160 @@ onMounted(() => {
 });
 </script>
 <template>
-  <div>
+  <div v-if="loading">
+    <div
+      class="absolute top-0 left-0 h-[380px] w-full bg-gradient-to-t from-dark-1000 to-transparent"
+    ></div>
+    <div
+      class="max-w-screen-xl px-4 flex relative gap-8 mx-auto flex-col pt-12 w-full"
+    >
+      <section
+        class="gap-8 mb-20 mx-auto flex justify-between items-center w-full"
+      >
+        <RouterLink to="/" class="bg-primary-1000 w-max p-2 rounded-xl">
+          <svg width="20" height="20" class="fill-white">
+            <use href="/sprite.svg#icon-arrow-left" />
+          </svg>
+        </RouterLink>
+      </section>
+      <div class="flex flex-col md:flex-row md:gap-16">
+        <div
+          class="flex md:flex-col py-4 w-full md:w-3/12 gap-5 order-2 flex-col-reverse md:order-none"
+        >
+          <section>
+            <div class="w-full skeleton rounded-xl h-[400px]"></div>
+          </section>
+          <section
+            class="flex w-full gap-4 flex-wrap h-max content-start grow justify-between opacity-80"
+          >
+            <div class="skeleton w-[80px] h-[80px] rounded-full"></div>
+            <div class="flex flex-col gap-4">
+              <h2 class="skeleton w-[120px] h-[24px] rounded-xl"></h2>
+              <p class="skeleton w-[120px] h-[24px] rounded-xl"></p>
+            </div>
+            <div class="text-xs h-max leading-6 gap-4 flex flex-col">
+              <p class="skeleton w-[120px] h-[24px] rounded-xl"></p>
+              <p class="skeleton w-[120px] h-[24px] rounded-xl"></p>
+            </div>
+          </section>
+        </div>
+        <div class="w-full md:w-8/12">
+          <section>
+            <div class="flex py-4 gap-5">
+              <div class="py-2 grow flex flex-col justify-between">
+                <div class="w-full flex flex-col gap-4">
+                  <div class="flex w-full items-center justify-between">
+                    <h1 class="h-[4rem] w-2/3 rounded-xl skeleton"></h1>
+           
+
+                  </div>
+                  <div class="flex gap-4">
+                  
+                    <span
+                      class="skeleton w-[100px] h-8 rounded-xl text-xs font-ligth"
+                    >
+                    </span>
+                    <span
+                      class="skeleton w-[100px] h-8 rounded-xl text-xs font-ligth"
+                    >
+                    </span>
+                  </div>
+                  <p
+                    style="word-spacing: 0.25rem"
+                    class="leading-6 text-white  skeleton w-full h-[24px] rounded-xl  text-sm"
+                  >
+                   
+                  </p>
+                  <p
+                    style="word-spacing: 0.25rem"
+                    class="leading-6 text-white  skeleton w-full h-[24px] rounded-xl  text-sm"
+                  >
+                   
+                  </p>     <p
+                    style="word-spacing: 0.25rem"
+                    class="leading-6 text-white  skeleton w-1/2 h-[24px] rounded-xl  text-sm"
+                  >
+                   
+                  </p>
+                  <div class="flex gap-3">
+                    <span
+                      class="skeleton w-[70px] h-8 rounded-xl text-xs font-ligth"
+                    >
+                    </span>
+                    <span
+                      class="skeleton w-[70px] h-8 rounded-xl text-xs font-ligth"
+                    >
+                    </span>
+                    <span
+                      class="skeleton w-[70px] h-8 rounded-xl text-xs font-ligth"
+                    >
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section
+            class="gap-8 mb-20 mx-auto flex-col justify-between items-start w-full hidden md:flex"
+          >
+          <h2 class="h-[2rem] w-[100px] rounded-xl skeleton"></h2>
+            <ul
+              class="divide-y  divide-primary-1000 flex w-full flex-col"
+            >
+              <li class="flex py-4 gap-4">
+                <h3 class="font-bold shrink-0 skeleton w-[150px] h-[1.5rem] rounded-xl"></h3>
+                <p class="grow skeleton h-[1.5rem] rounded-xl opacity-60"></p>
+              </li>
+            <li class="flex py-4 gap-4">
+                <h3 class="ffont-bold shrink-0 skeleton w-[150px] h-[1.5rem] rounded-xl"></h3>
+                <p class="grow skeleton h-[1.5rem] rounded-xl opacity-60"></p>              </li>
+            <li class="flex py-4 gap-4">
+                <h3 class="font-bold shrink-0 skeleton w-[150px] h-[1.5rem] rounded-xl"></h3>
+                <p class="grow skeleton h-[1.5rem] rounded-xl opacity-60"></p>              </li>
+            <li class="flex py-4 gap-4">
+                <h3 class="font-bold shrink-0 skeleton w-[150px] h-[1.5rem] rounded-xl"></h3>
+                <p class="grow skeleton h-[1.5rem] rounded-xl opacity-60"></p>              </li>
+            <li class="flex py-4 gap-4">
+                <h3 class="font-bold shrink-0 skeleton w-[150px] h-[1.5rem] rounded-xl"></h3>
+                <p class="grow skeleton h-[1.5rem] rounded-xl opacity-60"></p>              </li>
+            <li class="flex py-4 gap-4">
+                <h3 class="font-bold shrink-0 skeleton w-[150px] h-[1.5rem] rounded-xl"></h3>
+                <p class="grow skeleton h-[1.5rem] rounded-xl opacity-60"></p>              </li>
+            </ul>
+          </section>
+        </div>
+        <section
+          class="gap-8 mb-20 mx-auto flex flex-col justify-between items-start w-full md:hidden order-4"
+        >
+        <h2 class="h-[2rem] w-[100px] rounded-xl skeleton"></h2>
+            <ul
+              class="divide-y  divide-primary-1000 flex w-full flex-col"
+            >
+              <li class="flex py-4 gap-4">
+                <h3 class="font-bold shrink-0 skeleton w-[150px] h-[1.5rem] rounded-xl"></h3>
+                <p class="grow skeleton h-[1.5rem] rounded-xl opacity-60"></p>
+              </li>
+            <li class="flex py-4 gap-4">
+                <h3 class="ffont-bold shrink-0 skeleton w-[150px] h-[1.5rem] rounded-xl"></h3>
+                <p class="grow skeleton h-[1.5rem] rounded-xl opacity-60"></p>              </li>
+            <li class="flex py-4 gap-4">
+                <h3 class="font-bold shrink-0 skeleton w-[150px] h-[1.5rem] rounded-xl"></h3>
+                <p class="grow skeleton h-[1.5rem] rounded-xl opacity-60"></p>              </li>
+            <li class="flex py-4 gap-4">
+                <h3 class="font-bold shrink-0 skeleton w-[150px] h-[1.5rem] rounded-xl"></h3>
+                <p class="grow skeleton h-[1.5rem] rounded-xl opacity-60"></p>              </li>
+            <li class="flex py-4 gap-4">
+                <h3 class="font-bold shrink-0 skeleton w-[150px] h-[1.5rem] rounded-xl"></h3>
+                <p class="grow skeleton h-[1.5rem] rounded-xl opacity-60"></p>              </li>
+            <li class="flex py-4 gap-4">
+                <h3 class="font-bold shrink-0 skeleton w-[150px] h-[1.5rem] rounded-xl"></h3>
+                <p class="grow skeleton h-[1.5rem] rounded-xl opacity-60"></p>              </li>
+            </ul>
+        </section>
+      </div>
+    </div>
+  </div>
+  <div v-else>
     <div class="absolute top-0 left-0 w-full z-0">
       <img
         :src="data.images[0]"
@@ -225,51 +378,3 @@ onMounted(() => {
   </div>
 </template>
 <style scoped></style>
-
-<!-- 
-<BlurHash
-:src="data.poster"
-blurHash="LEHLh[WB2yk8pyoJadR*.7kCMdnj"
-:width="1"
-:height="125"
-:alt="`${data.title}-image`"
-/> -->
-<!-- 
-<div v-if="loading"
-class="max-w-screen-xl px-4 flex relative gap-8 mx-auto flex-col pt-12 w-full"
->
- <section class="gap-8 mb-20 mx-auto flex justify-between items-center w-full">
-  <RouterLink to="/" class="bg-primary-1000 w-max p-2 rounded-xl">
-    <svg width="20" height="20" class="fill-white">
-      <use href="/sprite.svg#icon-arrow-left" />
-    </svg>
-  </RouterLink>
-</section>
- <div  class="flex py-4 border-b gap-5 border-primary-1000">
-<div  class="skeleton w-[125px] h-[125px] rounded-md">
-
-</div>
-<div class="py-2 grow flex flex-col justify-between h-[125px]">
-  <div class="w-full">
-    <div class="flex w-full items-center justify-between">
-      <div class="skeleton w-1/2 h-6 rounded-md"></div>
-      <div class="skeleton w-8 h-8 rounded-md"></div>
-    </div>
-    <div>
-      <span class="skeleton w-1/6 h-6 rounded-md"> </span>
-    </div>
-  </div>
-  <div class="flex items-center gap-3">
-    <span class="skeleton w-1/12 h-6 rounded-md"> </span>
-    <span
-      class="text-md w-[6px] h-[6px] rounded-full bg-primary-1000"
-    ></span>
-    <span class="skeleton w-1/12 h-6 rounded-md"> </span>
-    <span
-      class="text-md w-[6px] h-[6px] rounded-full bg-primary-1000"
-    ></span>
-    <div class="skeleton w-1/12 h-6 rounded-md"></div>
-  </div>
-</div>
-</div>
-</div> -->
